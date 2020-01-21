b88c8e14ffb4d9bf69933eacf9a9192723a0148c
Adedamola Wuraola <32375311+awur978@users.noreply.github.com>
2020-01-22 07:46:35 +1300

    Update apply.m

--- +sq_rbf/apply.m
+++ +sq_rbf/apply.m
@@ -1,7 +1,5 @@
 function a = apply(n,param)
-%RADBAS.APPLY transfer function to inputs
 
-% Copyright 2012-2015 The MathWorks, Inc.
 
 			m = abs(n); 
 			a = (m < 1) .* (1 - m.^2/2)             + ...
