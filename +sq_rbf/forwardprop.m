4dc6479792262424393138ef1ae7e9c1fccd4daf
Adedamola Wuraola <32375311+awur978@users.noreply.github.com>
2020-01-23 08:37:46 +1300

    Update forwardprop.m

--- +sq_rbf/forwardprop.m
+++ +sq_rbf/forwardprop.m
@@ -1,10 +1,6 @@
 function da = forwardprop(dn,m,a,param)
-%RADBAS.FORWARDPROP Forward propagate derivatives from input to output.
+%SQ-RBF.FORWARDPROP Forward propagate derivatives from input to output.
 
-% Copyright 2012-2015 The MathWorks, Inc.
-
-  
-     % m = abs(n); 
 			d = (m < 1& m >-2) .* (-m)             + ...
                   (m >=1 & m < 2)   .* ((m-2));
 			
